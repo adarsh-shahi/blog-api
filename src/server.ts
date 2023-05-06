ef78d32152281d2b7cfdb390deb67764dbe1a768
adarsh-shahi <adarshshahi1009@gmail.com>
2023-05-07 00:08:48 +0530

    response origin *

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import { app } from "./app";
 
-// app.use(function (req, res, next) {
-// 	res.header("Access-Control-Allow-Origin", "http://192.168.0.111:3001"); // update to match the domain you will make the request from
-// 	// res.header(
-// 	// 	"Access-Control-Allow-Headers",
-// 	// 	"Origin, X-Requested-With, Content-Type, Accept"
-// 	// );
-// 	next();
-// });
+
 
 const PORT = parseInt(process.env.PORT || "") || 8000;
 const HOST: string = `localhost`;
