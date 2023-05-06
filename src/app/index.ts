20ec3e716b6463d133f875c3facbc38e705f5f6e
adarsh-shahi <adarshshahi1009@gmail.com>
2023-05-07 00:14:14 +0530

    response origin *

--- src/app/index.ts
+++ src/app/index.ts
@@ -7,7 +7,12 @@ const app = express();
 
 app.use(cors());
 app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Credentials", "true");
 	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
 	next();
 });
 app.use(express.json());
